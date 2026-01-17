1e32c637918007ca55c0bc76aac77f03f84afe47
Kritt07 <dum98415@gmail.com>
2026-01-17 20:41:00 +0500

    написал api.js с запрсами getUserInfo, getCardList

---
+++ src/scripts/components/api.js
@@ -0,0 +1,24 @@
+const config = {
+  baseUrl: "https://mesto.nomoreparties.co/v1/apf-cohort-202",
+  headers: {
+    authorization: "ce314302-0f3f-4cf5-aaac-ae528920b032",
+    "Content-Type": "application/json",
+  },
+};
+
+/* Проверяем, успешно ли выполнен запрос, и отклоняем промис в случае ошибки. */
+const getResponseData = (res) => {
+  return res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`);
+};
+
+export const getUserInfo = () => {
+  return fetch(`${config.baseUrl}/users/me`, { // Запрос к API-серверу
+    headers: config.headers, // Подставляем заголовки
+  }).then(getResponseData);  // Проверяем успешность выполнения запроса
+};
+
+export const getCardList = () => {
+    return fetch(`${config.baseUrl}/cards`, {
+        headers: config.headers,
+    }).then(getResponseData);
+}
